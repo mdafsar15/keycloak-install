28e6577832d630e338fb293e08e001dab7c2e05c
Afsar Ansari <mdafsaransari720@gmail.com>
2022-06-24 11:50:47 +0530

    keycloak

--- installkeycloak.sh
+++ installkeycloak.sh
@@ -1,7 +1,7 @@
 script0='curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl";
          curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256";
 	 echo "$(cat kubectl.sha256) kubectl" | sha256sum --check;
-         sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl -y;
+         sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl;
          kubectl version --client;';
 
 sshpass -p Welcome1 ssh -o StrictHostKeyChecking=no root@192.168.0.210 $script0;
