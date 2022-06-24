94426a986b64d0abcacf9ef30887abd1d52a0e07
Afsar Ansari <mdafsaransari720@gmail.com>
2022-06-24 11:36:33 +0530

    keycloak

--- installkeycloak.sh
+++ installkeycloak.sh
@@ -1,4 +1,5 @@
-script0='curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl";';
+script0='curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl";
+         curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"';
 
 sshpass -p Welcome1 ssh -o StrictHostKeyChecking=no root@192.168.0.210 $script0;
 
